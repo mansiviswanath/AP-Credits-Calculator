50ac413dd5f8b064e343882812682efe82400d64
mansiviswanath <mansi.viswanath09@gmail.com>
2026-05-24 11:56:56 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Analytics } from '@vercel/analytics/next';
 import App from './App.jsx'
 import './index.css'
+// Import the React-specific Analytics component
+import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    {/* Place the component inside your root render */}
+    <Analytics />
   </React.StrictMode>,
 )
+
