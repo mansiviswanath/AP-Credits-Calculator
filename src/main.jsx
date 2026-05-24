f1c1666d6d27c6f2e2c2dcac93ab560e23ca1685
mansiviswanath <mansi.viswanath09@gmail.com>
2026-05-23 21:34:45 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import React from 'react' 
 import ReactDOM from 'react-dom/client' 
-import { Analytics } from "@vercel/analytics/next" 
 import App from './App.jsx' 
-import './index.css' ReactDOM.createRoot(document.getElementById('root')).render( <React.StrictMode> <App /> </React.StrictMode>, )
+import './index.css' 
+
+ReactDOM.createRoot(document.getElementById('root')).render( 
+  <React.StrictMode> 
+    <App /> 
+  </React.StrictMode>, 
+)
